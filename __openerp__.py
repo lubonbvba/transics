cbc5fd7c640b8876ccfeae61dd86fd18dffae61b
Luc Bonjean <luc@lubon.be>
2017-12-26 21:14:24 +0100

    Updates for transics

--- __openerp__.py
+++ __openerp__.py
@@ -16,14 +16,14 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'data/transics_cron.xml',
         'views/res_config.xml',
         'views/transics.xml',
